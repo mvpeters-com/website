3e677c76d01db029ec7f0e0f59e87494f81f207f
simon <simon@spaced.be>
2024-04-11 14:51:34 +0200

    tweak animations

--- src/Animation.jsx
+++ src/Animation.jsx
@@ -2,6 +2,12 @@ import Spline from '@splinetool/react-spline';
 
 export default function Animation() {
   return (
-      <Spline scene="https://prod.spline.design/1h5V2AEnzh10dGAd/scene.splinecode"/>
+      <Spline style={{
+        width: '90%',
+        height: '90%',
+        position: 'absolute',
+        top: '5%',
+        left: '5%',
+      }} scene="https://prod.spline.design/1h5V2AEnzh10dGAd/scene.splinecode"/>
   );
 }

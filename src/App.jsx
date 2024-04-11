cbe508e4b89816fe63512591ef717adf8b39d951
simon <simon@spaced.be>
2024-04-11 15:37:32 +0200

    fix stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Animation from "./Animation.jsx";
 
 export default function App() {
-  return <div className={'container'}>
-    <div className={'text email'}>
-      <a href={'mailto:simon@mvpeters.com'}>simon@mvpeters.com</a>
+  return <>
+    <div className={'container'}>
+      <div className={'text email'}>
+        <a href={'mailto:simon@mvpeters.com'}>simon@mvpeters.com</a>
+      </div>
+
+      <div className={'text phone'}>
+        <a href={'tel:+32476399887'}>32476399887</a>
+      </div>
+
     </div>
 
-    <div className={'text phone'}>
-      <a href={'tel:+32476399887'}>32476399887</a>
-    </div>
     <Animation/>
-  </div>
-
+  </>
 }

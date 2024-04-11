0cda3acdd3c19eded6c8740e0863ce14818d3c8e
simon <simon@spaced.be>
2024-04-11 15:27:25 +0200

    add animation

--- src/Animation.jsx
+++ src/Animation.jsx
@@ -1,6 +1,37 @@
 import Spline from '@splinetool/react-spline';
+import {useEffect, useState} from "react";
 
 export default function Animation() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const [isMobile, setIsMobile] = useState(false);
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [setWindowSize]);
+
+  useEffect(() => {
+    if (windowSize.width < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, [windowSize.width]);
+
   return (
       <Spline style={{
         width: '90%',
@@ -8,6 +39,6 @@ export default function Animation() {
         position: 'absolute',
         top: '5%',
         left: '5%',
-      }} scene="https://prod.spline.design/1h5V2AEnzh10dGAd/scene.splinecode"/>
+      }} scene={isMobile ? 'https://prod.spline.design/PX7YtOY1JXXr-gFN/scene.splinecode' : 'https://prod.spline.design/1h5V2AEnzh10dGAd/scene.splinecode'}/>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Animation from "./Animation.jsx";
 
 export default function App() {
-  return (
-      <Animation/>
-  );
+  return <div className={'container'}>
+    <div className={'text email'}>
+      <a href={'mailto:simon@mvpeters.com'}>simon@mvpeters.com</a>
+    </div>
+
+    <div className={'text phone'}>
+      <a href={'tel:+32476399887'}>32476399887</a>
+    </div>
+    <Animation/>
+  </div>
+
 }

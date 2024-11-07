8e295f5bc7f51827da2ad4515f978db84c95ddcd
simon <simon@spaced.be>
2024-11-07 12:33:15 +0100

    add astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
   integrations: [react(), tailwind({
       applyBaseStyles: false,
-    })]
+    })],
+
+  adapter: vercel()
 });
